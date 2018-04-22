62197492094593ab04c0cc69de3e6163e0068546
Kjetil Ravnås <kjetil@doublethink.no>
2018-04-23 01:57:52 +0200

    New functions
    - added function for tooltip
    - added function for sliders
    - added function for buttons
    - added calculation function for price
    - added section for button actions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-$(function(){
+
+/* Function for changing background color on checkboxes */
+$(function (){
   var checkboxs = $('input[type=checkbox]');
   
   checkboxs.each(function(){
@@ -13,4 +15,61 @@ $(function(){
      $(this).parent().removeClass('customCheckboxChecked');
     }
   });
-})
+});
+
+
+/* Function for enabling tooltip. Requires popper.js */
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+
+/* Activate Slider Component in Bootstrap */
+// CPU-SLIDER
+$("#cpu").slider();
+$("#cpu").on("slide", function(slideEvt) {
+	$("#cpuSliderVal").text(slideEvt.value); });
+
+// RAM-SLIDER
+$("#ram").slider();
+$("#ram").on("slide", function(slideEvt) {
+	$("#ramSliderVal").text(slideEvt.value);
+});
+
+// RAM-SLIDER
+$("#ssd").slider();
+$("#ssd").on("slide", function(slideEvt) {
+	$("#ssdSliderVal").text(slideEvt.value);
+});
+
+/* Calculate value of sliders and update price */
+$(document).change(function () {
+		
+		var price_cpu = 0.1;
+		var price_ram = 0.1;
+		var price_ssd = 0.1;
+		var cpu = document.getElementById("cpu").value;
+		var ram = document.getElementById("ram").value;
+		var ssd = document.getElementById("ssd").value;
+		var total = (parseFloat(cpu) * parseFloat(price_cpu)) + (parseFloat(ram) * parseFloat(price_ram)) + (parseFloat(ssd) * parseFloat(price_ssd));
+		var amount = total.toFixed(2);
+
+        	$("#price-number").text('$' + amount).append('<span class="font-weight-normal text-dark"><small style="font-size:40%;"> /mo</small></span>');
+});
+
+/* ----------------------- BUTTONS ACTIONS ------------------------ */
+/* Trigger Demo Button and goto url */
+$("#demo-button").on('click', function(){
+     window.location = "demo.html";    
+});
+
+/* Trigger More Featurea Button and goto url */
+$("#more-features-button").on('click', function(){
+     window.location = "features.html";    
+});
+
+
+
+
+
+
